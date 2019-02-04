76d4886eb14dcad1fe4e1e2becaa5ceb158dd778
Hasan <hasanalrasyid.har@gmail.com>
2019-02-04 10:41:04 +0900

    add 2 variables

--- app/Main.hs
+++ app/Main.hs
@@ -12,9 +12,11 @@ setCrateRoot []
 main = do
   putStrLn "Haskell: Hello. Enter a number:"
   x <- readLn
+  z <- readLn
   y <- [rustIO| i32 {
     println!("Rust: Your number is {}", $(x: i32));
     $(x: i32) + 1
+    $(z: i32) + 2
     $(ret) = 32
   } |]
   putStrLn $ "Haskell: Rust says number plus 1 is " ++ show y
