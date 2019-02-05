ad65ef27a8dd68f76a135f29082216cf5b938215
Hasan <hasanalrasyid.har@gmail.com>
2019-02-05 11:02:45 +0900

    remove shadowing

--- ghcid.sh
+++ ghcid.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]; then
-  tipe="-Wno-missing-signatures -Wno-orphans -Wno-unused-matches -Wno-unused-local-binds -Wno-unused-top-binds"
+  tipe="-Wno-missing-signatures -Wno-orphans -Wno-unused-matches -Wno-unused-local-binds -Wno-unused-top-binds -Wno-unused-imports"
 else
   tipe=""
 fi
-LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH ghcid -c "stack ghci --main-is app --only-main --ghci-options '-Wall $tipe'"
+LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH ghcid -c "stack ghci --main-is inline-fortran:exe:inline-fortran-exe --only-main --ghci-options '-Wall $tipe'"
