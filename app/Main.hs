1e141912dd782b2e2cf736f23de396573565138b
aku <aku@ASUS>
2019-02-26 18:21:52 +0900

    back to exRust

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Language.Inline
+import Language.Rust.Inline
 import Data.Int
 
 extendContext basic
@@ -12,17 +12,9 @@ setCrateRoot []
 main = do
   putStrLn "Haskell: Hello. Enter a number:"
   x <- readLn
-  z <- readLn
-  y <- [rustIO| i32 { double precision {
-      double precision a,b,c,eps
-      a = 4.0d0/3.0d0
-   10 b = a - 1.0d0
-      c = b + b + b
-      eps = dabs(c-1.0d0)
-      if (eps .eq. 0.0d0) go to 10
-      $(ret) = eps*dabs(x)
-      return
-      }
-      } |]
+  y <- [rustIO| i32 {
+    println!("Rust: Your number is {}", $(x: i32));
+    $(x: i32) + 1
+  } |]
   putStrLn $ "Haskell: Rust says number plus 1 is " ++ show y
 
