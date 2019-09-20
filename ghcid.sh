f896846a3047bbbe6d5b62e7f47625810f261697
aku <aku@ASUS>
2019-09-20 23:42:43 +0900

    test

--- ghcid.sh
+++ ghcid.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]; then
-  tipe="-Wno-missing-signatures -Wno-orphans -Wno-unused-matches -Wno-unused-local-binds -Wno-unused-top-binds -Wno-unused-imports -Wno-name-shadowing"
+  tipe="-Wno-missing-signatures -Wno-orphans -Wno-unused-matches -Wno-unused-local-binds -Wno-unused-top-binds -Wno-unused-imports -Wno-name-shadowing -Wmissing-methods"
 else
-  tipe=""
+  tipe="-Wall"
 fi
-LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH ghcid -c "stack ghci --main-is inline-fortran:exe:x --only-main --ghci-options '-Wall $tipe'"
+LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH ghcid -c "stack ghci --main-is inline-fortran:exe:x --only-main --ghci-options '$tipe'"
