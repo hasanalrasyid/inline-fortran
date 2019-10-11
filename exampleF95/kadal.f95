9d3bec9e41c10d4b77f3de3145cf5e57285169a9
aku <aku@ASUS>
2019-10-11 22:11:25 +0900

    tambah example.. bikin sesuai default

---
+++ exampleF95/kadal.f95
@@ -0,0 +1,7 @@
+subroutine square_cube(i,isquare, icube,iasdfljaksdfhalsdjkfhasldkfashfasdfsafsdfasdfs)
+  integer, intent(in) :: i
+  integer, intent(out) :: isquare, icube
+  isquare = i ** 2
+  icube = i ** 3
+end subroutine square_cube
+
---
+++ exampleF95/main.f95
@@ -0,0 +1,6 @@
+program main
+  implicit none
+  integer :: isq, icub
+  call square_cube(4,isq,icub)
+  print *, "i,i^2,i^3=",4,isq,icub
+end program main
