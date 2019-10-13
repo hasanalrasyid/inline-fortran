5dd1ef686a1c5e862d2f3fbc609ef2d2d09a6c48
aku <aku@ASUS>
2019-10-13 17:07:06 +0900

    test

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,21 @@ module Main where
 import Language.Rust.Inline
 import Data.Int
 import Language.C.Inline
+import Foreign
 
 main = do
   putStrLn "Haskell: Hello. Enter a number:"
   let i = 55
   r <- return ()
   rr <- withPtr_ $ \x -> do
+    poke x 75
     [rustIO|
       print *, "adalah dianya yang sepertinya"
       print *, "adalah dianya yang sepertinya ",x
       k = $(x : i32) + 5
-      x = 5
+      x = x + k
     |]
     putStrLn $ "Haskell: Rust says in withPtr x=" ++ show x
+    xContent <- peek x
+    putStrLn $ "Haskell: Rust says in withPtr x=" ++ show xContent
   putStrLn $ "Haskell: Rust says number plus 1 is " ++ show rr
