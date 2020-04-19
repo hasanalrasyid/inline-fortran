ad0644bb463998f191acdb4c7aa372f18ef44b51
aku <aku@ASUS>
2020-04-19 23:06:57 +0900

    nix integration

---
+++ shell-stack.nix
@@ -0,0 +1,23 @@
+{ reflex-platform ? import ./reflex-platform {
+  config.android_sdk.accept_license = true;
+  config.allowBroken = true;
+  config.extraoptions = "
+    keep-outputs = true
+    keep-derivations = true
+  ";
+  config.doHaddock = false;
+  }
+, withHoogle ? false
+}:
+with reflex-platform.nixpkgs;
+let
+nixpkgs = reflex-platform.nixpkgs;
+ghc = nixpkgs.ghc;
+in
+nixpkgs.haskell.lib.buildStackProject {
+  inherit ghc;
+  name = "inline-fortran";
+  buildInputs = [ git hlint protobuf haskellPackages.stylish-haskell zlib ];
+  doHaddock = false;
+  doCheck = false;
+}
