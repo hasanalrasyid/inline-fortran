d4e9aa873bf36b49c132bfc715f063e4326fa024
aku <aku@ASUS>
2020-04-27 12:25:00 +0900

    ghcid needed

--- ghcid.sh
+++ ghcid.sh
@@ -4,4 +4,6 @@ else
   tipe="-Wall"
 fi
 #LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH stack exec ghcid -- -c "stack ghci --main-is inline-fortran:test:i --only-main --ghci-options '$tipe'"
-LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH nix-shell shell-stack.nix --run "stack exec ghcid -- -c \"stack ghci --main-is inline-fortran:test:i --only-main --ghci-options '$tipe'\""
+#LD_LIBRARY_PATH=./foreign/lib:$LD_LIBRARY_PATH nix-shell shell-stack.nix --run "stack exec ghcid -- -c \"stack ghci --main-is inline-fortran:test:i --only-main --ghci-options '$tipe'\""
+ghcid --command 'stack ghci --ghci-options=fbyte-code --no-nix-pure inline-fortran:lib inline-fortran:test:i'  --restart inline-fortran.cabal --restart stack.yaml -W
+
