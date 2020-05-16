3443bc899b10fc2916202231b3b368a30332982c
aku <aku@ASUS>
2020-05-16 23:03:42 +0900

    try to bump

--- shell-stack.nix
+++ shell-stack.nix
@@ -3,6 +3,7 @@
 }:
 let
 config = {
+  doCheck = false;
   allowBroken = true;
   packageOverrides = pkgs: rec {
       haskellPackages = pkgs.haskell.packages.ghc8101;
@@ -10,15 +11,20 @@ config = {
   };
 pkgs = (import ./nixpkgs {inherit config;}).pkgs;
 ghc = pkgs.haskell.packages.ghc8101.ghcWithPackages (ps: with ps; []);
+HsYAML = pkgs.haskell.lib.addBuildDepend pkgs.haskellPackages.HsYAML (with pkgs;
+      [ haskellPackages.base_4_12_0_0 ]);
+
 this = rec {
     inherit pkgs ghc;
   };
 project =
 pkgs.haskell.lib.buildStackProject {
   name = "inline-fortran";
-  buildInputs = with pkgs; [ git hlint protobuf haskellPackages.stylish-haskell zlib gcc.cc gfortran gfortran.cc haskellPackages.happy haskellPackages.alex ghcid gdb ];
+  buildInputs = with pkgs; [ git hlint protobuf zlib gcc.cc gfortran gfortran.cc haskellPackages.happy haskellPackages.alex gdb
+  ];
   doHaddock = false;
   doCheck = false;
-};
+
+  };
 in
 this // project
